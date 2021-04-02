0ebf418c5308e3745aee1f893bb83f576ed8aa18
Frederson <fredemanduoliveira@hotmail.com>
2021-04-02 00:53:25 -0300

    refactor: Refatorando :art:

---
+++ 6-Semestre/Web1/Trabalho-Minha-Dieta/README.md
@@ -0,0 +1,3 @@
+# WEB 1 - 6 semestre.
+## Trabalhos da Disciplina de Web 1
+### Minha Dieta :banana: :apple: :grapes: :tangerine: :rice: :egg: :meat_on_bone:
--- 6-Semestre/Web1/Trabalho-Minha-Dieta/js/scrypt.js
+++ 6-Semestre/Web1/Trabalho-Minha-Dieta/js/scrypt.js
@@ -1,17 +1,13 @@
 function addIngrediente() {
     var elemento_pai = document.getElementById('ingre');
 
-    // Criar elemento
     var span = document.createElement('span');
 
     var vlr = document.getElementById('vlrIngre').value;
     var texto = document.createTextNode(vlr+ ', ');
-
-    // Anexar o nó de texto ao elemento span
     span.appendChild(texto);
-
-    // Agora sim, inserir (anexar) o elemento filho (span) ao elemento pai
     elemento_pai.appendChild(span);
+    document.getElementById('vlrIngre').value = '';
 }
 
 function addAlert(msg){
