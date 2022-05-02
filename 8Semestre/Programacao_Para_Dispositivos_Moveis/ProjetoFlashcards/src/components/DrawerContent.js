804c501d5151c24410a08306e61710377b3111d0
frede122 <fredemanduoliveira@hotmail.com>
2022-05-02 01:55:20 -0300

    fix: Correção menu lateral :bug:

--- 8Semestre/Programacao_Para_Dispositivos_Moveis/ProjetoFlashcards/src/components/DrawerContent.js
+++ 8Semestre/Programacao_Para_Dispositivos_Moveis/ProjetoFlashcards/src/components/DrawerContent.js
@@ -4,14 +4,28 @@ import { View, Text, StyleSheet, Image } from "react-native";
 import { DrawerContentScrollView, DrawerItemList, DrawerItem } from "@react-navigation/drawer";
 import Profile from "./Profile";
 import {UserImage} from '../assets/images'
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function DrawerContent(props) {
     return (
       <DrawerContentScrollView {...props}>
         <Profile imagem={UserImage} texto="Marcelo dos Santos" />
-
-        <DrawerItemList style={{labelStyle : {color:'white'}}}{...props} />
+        <TouchableOpacity  onPress={()=>{props.navigation.navigate('MinhaColecoes')}}>
+          <Text style={styles.text}>Minhas Coleções</Text>
+        </TouchableOpacity>
+        <TouchableOpacity  onPress={()=>{props.navigation.navigate('MinhaColecoes')}}>
+          <Text style={styles.text}>Logout</Text>
+        </TouchableOpacity>
       </DrawerContentScrollView>
     );
   }
+
+  const styles = StyleSheet.create({
+
+    text: {
+      color: 'white',
+      fontSize: 20,
+      margin: 10
+    }
+  })
 
